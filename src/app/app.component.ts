7d041df9f651aa5f2f220816f1b4ca7e69c691c5
Param Kamdar <kamdarp@uwindsor.ca>
2024-10-07 20:05:14 -0400

    Landing & footer added.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { PinCardComponent } from './components/pin-card/pin-card.component';
 import { PinListComponent } from './components/pin-list/pin-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PinCardComponent, PinListComponent, NavbarComponent],
+  imports: [RouterOutlet, PinCardComponent, PinListComponent, NavbarComponent, LandingPageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent }, // Default route to Landing Page
+    // { path: 'home', component: HomeComponent },
+    // { path: 'watch', component: WatchComponent },
+    // { path: 'explore', component: ExploreComponent },
+    // { path: 'login', component: LoginComponent },
+    // { path: 'signup', component: SignupComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }, // Fallback route if no match
+];
